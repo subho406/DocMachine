243cd5ae1e8aab5e4e6c74093a02c15c977de18e
amandavinci <aman@amandavinci.me>
2017-02-24 01:36:55 +0530

    Upload project to private repo

---
+++ docmachine.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def Welcome():
+    return app.send_static_file('index.html')
+
+@app.route('/myapp')
+def WelcomeToMyapp():
+    return 'Welcome again to my app running on Bluemix!'
+
+@app.route('/api/people')
+def GetPeople():
+    list = [
+        {'name': 'John', 'age': 28},
+        {'name': 'Bill', 'val': 26}
+    ]
+    return jsonify(results=list)
+
+@app.route('/api/people/<name>')
+def SayHello(name):
+    message = {
+        'message': 'Hello ' + name
+    }
+    return jsonify(results=message)
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
